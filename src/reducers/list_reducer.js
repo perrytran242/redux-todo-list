5c1f8214eb0e77ab83c73b1646e764e39d8b8db0
Perry Tran <ptran209@gmail.com>
2018-10-03 17:09:13 -0700

    Compoleted getting list from server

--- src/reducers/list_reducer.js
+++ src/reducers/list_reducer.js
@@ -4,9 +4,11 @@ const DEFAULT_STATE = {
     all: [],
     single: {},
 }
-
+// returns a promise and action cannot take in a promise so it throws and error.
 export default (state = DEFAULT_STATE, action) => {
     switch (action.type) {  
+        case types.GET_LIST_DATA:
+            return {...state, all: action.payload.data.todos };
         default: 
             return state;
     }
