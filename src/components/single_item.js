5c13299d98c1a1373897de2285709b86627b229c
Perry Tran <ptran209@gmail.com>
2018-10-05 14:53:15 -0700

    created complete button functionality

--- src/components/single_item.js
+++ src/components/single_item.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getSingleItem } from '../actions';
+import { getSingleItem, clearSingleItem, toggleComplete } from '../actions';
 
 class SingleItem extends Component {
     componentDidMount() {
         console.log('Item ID:', this.props.match.params.itemId);
         this.props.getSingleItem(this.props.match.params.itemId);
     }
+    componentWillUnmount() {    
+        this.props.clearSingleItem();
+    }
     render(){
-        // console.log('Single Item:', this.props.item);
-        const { item } = this.props;
-        return (
+        console.log('Single Item:', this.props.item);
+        const { item, toggleComplete, match: { params } } = this.props;
+        return (    
             <div className="center">
                 <h1>To Do Item</h1>
                 <h3>Title: {item.title}</h3>
                 <h4>Details: {item.details}</h4>
+                <h4 className={`${item.complete ? 'green-text' : 'red-text'}`}>{item.complete ? 'Item has been completed.' : 'Item NOT completed'}</h4>
+                <button onClick={() => toggleComplete(params.itemId)} className={`btn ${item.complete ? 'red' : 'green'}`}>{item.complete ? 'Remove Complete' : 'Complete Item'}</button>
             </div>
-        );
+        ); 
     }
 }
 
@@ -28,5 +33,7 @@ function mapStateToProps(state){
 }
 
 export default connect(mapStateToProps, {
-    getSingleItem: getSingleItem
+    getSingleItem: getSingleItem,
+    clearSingleItem: clearSingleItem,
+    toggleComplete: toggleComplete,
 })(SingleItem);
